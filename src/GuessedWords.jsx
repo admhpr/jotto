303256470eee81ba16fedc7afe2a5ebb27587d79
Adam Harpur <adam@harpur.io>
2018-11-16 09:35:21 -0600

    added initial tests for guessed words

--- src/GuessedWords.jsx
+++ src/GuessedWords.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { fchmodSync } from "fs";
 
 const GuessedWords = (props) => {
+    let contents;
+    if(props.guessedWords.length === 0){
+      contents  = (
+        <span data-test="guess-instructions">
+          Try to guess the secret word!
+        </span>
+      )
+    }
     return (
-        <div/>
+        <div data-test="component-guessed-words">
+          {contents}
+        </div>
     )
 };
 
 GuessedWords.propTypes = {
-    guessedWords: PropTypes.arrayOf(
-        PropTypes.shape({
-            guessedWord: PropTypes.string.isRequired,
-            letterMatchCount: PropTypes.number.isRequired,
-        })
-    ).isRequired
-}
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired
+    })
+  ).isRequired
+};
 export default GuessedWords;
--- src/__tests__/GuessedWords.test.js
+++ src/__tests__/GuessedWords.test.js
@@ -15,12 +15,27 @@ const defaultProps = {
 const setup = (props={}) => {
     const setupProps = {...defaultProps, ...props};
     return shallow( <GuessedWords {...setupProps}/> )
-}
-
-test('renders without error', () => {
-    const wrapper = shallow( <GuessedWords / > );
-});
+};
 
 test('does not throw any warning with expected props', () => {
     checkProps(GuessedWords, defaultProps);
+})
+
+describe('if there are no words guessed', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = setup({ guessedWords: [] })
+    })
+    test('renders without error', () => {
+        const component = findByAttr(wrapper, 'component-guessed-words');
+        expect(component.length).toBe(1);
+    })
+    test('renders instructions to guess a word', () => {
+        const instructions = findByAttr(wrapper, 'guess-instructions');
+        expect(instructions.text().length).not.toBe(0)
+    })
+})
+
+describe('if there are words guessed', () => {
+
 })
