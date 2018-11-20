e917f51793bf36d1f46db13bf96eaf035fb31fcf
Adam Harpur <adam@harpur.io>
2018-11-20 06:00:00 -0600

    added components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
+
+import GuessedWords from './GuessedWords';
+import Congrats from './Congrats';
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1> Hello, World! </h1>
+            <h1>Jotto</h1>
+            <Congrats succes={false}></Congrats>
+            <GuessedWords guessedWords={[]}></GuessedWords>
             </div>
         );
     }
