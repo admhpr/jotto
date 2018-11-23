a37c93e5fc2b4cb7214a5bc96f80e853f298418b
Adam Harpur <adam@harpur.io>
2018-11-23 07:48:39 -0600

    stubbed out input component and test

---
+++ src/__tests__/Input.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    findByAttr,
+    checkProps
+} from './testUtils';
+import Input from '../Input';
+import {
+    shallow
+} from 'enzyme';
+
+test('', () => {
+    
+})
