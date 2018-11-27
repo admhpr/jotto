8f2359fe57486785eda5bbbdde187fc96ebcdd08
Adam Harpur <adam@harpur.io>
2018-11-27 06:43:12 -0600

    added guessWord tests and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,10 @@
 export const actionTypes = {
     CORRECT_GUESS: 'CORRECT_GUESS',
+    GUESS_WORD: 'GUESS_WORD',
 }
 
-export function correctGuess(){
-    return { type: actionTypes.CORRECT_GUESS };
+export const guessWord = (guessedWord) => {
+    return function(dispatch, getState){
+
+    }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux';
 import success from './success_reducer';
+import guessedWords from './guessed_words_reducer';
 
 export default combineReducers({
     success,
+    guessedWords,
 })
 
