891816f1ff719b503818256b1f78789a2524f87a
Adam Harpur <adam@harpur.io>
2018-11-27 07:02:20 -0600

    added inital intergration tests

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux';
 import success from './success_reducer';
 import guessedWords from './guessed_words_reducer';
+import secretWord from './secret_word_reducer';
 
 export default combineReducers({
     success,
     guessedWords,
+    secretWord,
 })
 
