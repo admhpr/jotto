f75b2582333d694b00501e7e8bdc0f1dc08b5964
Adam Harpur <adam@harpur.io>
2018-11-28 07:18:39 -0600

    added code to green light tests on guess word action

--- src/__tests__/intergration.test.js
+++ src/__tests__/intergration.test.js
@@ -6,12 +6,14 @@ describe('guessWord action dispatcher', () => {
     const unsuccessfulGuess = 'train';
     describe('no guessed words', () => {
         let store;
-        const initialState = {secretWord}
+        const initialState = {
+            secretWord
+        };
         beforeEach(() => {
-            store = storeFactory(initialState)
-        })
+            store = storeFactory(initialState);
+        });
         test('updates state correctly for unsuccessful guess', () => {
-            store.dispatch(guessWord(unsuccessfulGuess))
+            store.dispatch(guessWord(unsuccessfulGuess));
             const newState = store.getState()
             const expectedState = {
                 ...initialState,
@@ -20,57 +22,63 @@ describe('guessWord action dispatcher', () => {
                     guessedWord: unsuccessfulGuess,
                     letterMatchCount: 3
                 }]
-            }
+            };
             expect(newState).toEqual(expectedState);
-          
-        })
+        });
         test('updates state correctly for successful guess', () => {
-            store.dispatch(guessWord(secretWord))
+            store.dispatch(guessWord(secretWord));
             const newState = store.getState()
             const expectedState = {
-                ...initialState,
+                secretWord,
                 success: true,
                 guessedWords: [{
                     guessedWord: secretWord,
-                    letterMatchCount: 5
-                }]
-            }
-            expect(newState).toEqual(expectedState)
-
-        })
-    })
+                    letterMatchCount: 5,
+                }],
+            };
+            expect(newState).toEqual(expectedState);
+        });
+    });
     describe('some guessed words', () => {
-        const guessedWords = [{ guessedWord: 'agile', letterMatchCount: 1} ];
+        const guessedWords = [{
+            guessedWord: 'agile',
+            letterMatchCount: 1
+        }];
         const initialState = {
-            guessedWords, 
+            guessedWords,
             secretWord
         }
         let store;
         beforeEach(() => {
             store = storeFactory(initialState);
-        }) 
+        })
         test('updates state correctly for unsuccessful guess', () => {
-            store.dispatch(guessWord(unsuccessfulGuess))
+            store.dispatch(guessWord(unsuccessfulGuess));
             const newState = store.getState();
             const expectedState = {
                 secretWord,
                 success: false,
-                guessedWords: [...guessedWords, {guessedWord: unsuccessfulGuess, letterMatchCount: 3}]
-            }
-            expect(newState).toEqual(expectedState)
-        })
+                guessedWords: [...guessedWords, {
+                    guessedWord: unsuccessfulGuess,
+                    letterMatchCount: 3
+                }]
+            };
+            expect(newState).toEqual(expectedState);
+        });
         test('updates state correctly for successful guess', () => {
-            store.dispatch(guessWord(unsuccessfulGuess))
+            store.dispatch(guessWord(secretWord));
             const newState = store.getState();
             const expectedState = {
                 secretWord,
                 success: true,
-                guessedWords: [...guessedWords, {
-                    guessedWord: secretWord,
-                    letterMatchCount: 5
-                }]
-            }
-            expect(newState).toEqual(expectedState)
-        })
-    })
-})
+                guessedWords: [...guessedWords,
+                    {
+                        guessedWord: secretWord,
+                        letterMatchCount: 5
+                    }
+                ]
+            };
+            expect(newState).toEqual(expectedState);
+        });
+    });
+});
