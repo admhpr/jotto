c697871a18353f99f4e8b16403e721b7a66cc6c6
Adam Harpur <adam@harpur.io>
2018-11-29 07:34:19 -0600

    added moxios tests for set secret word

---
+++ src/actions/__tests__/index.test.js
@@ -0,0 +1,29 @@
+import moxios from 'moxios';
+import {storeFactory} from '../../__tests__/testUtils';
+import {getSecretWord} from '../index';
+
+describe('getSecretWord actoin creator', () => {
+    beforeEach(() => {
+        moxios.install();
+    })
+    afterEach(() => {
+        moxios.uninstall();
+    })
+    test('adds response word to state', () => {
+        const secretWord = 'party'
+        const store = storeFactory()
+        moxios.wait(() => {
+            const request = moxios.requests.mostRecent();
+            request.respondWith({
+                status: 200,
+                response: secretWord,
+            })
+        })
+         
+        return store.dispatch(getSecretWord())
+        .then(() => {
+            const newState = store.getState()
+            expect(newState.secretWord).toBe(secretWord)
+        })
+    })
+}) 
