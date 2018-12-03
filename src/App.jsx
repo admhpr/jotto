36c14e205c588e9ca452cbd85071d7607bbbbb60
Adam Harpur <adam@harpur.io>
2018-12-03 06:24:26 -0600

    added input to connect component

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import {connect} from 'react-redux';
 import {getSecretWord} from './actions'
 import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
+import Input from "./Input";
 class App extends Component {
     render() {
         return (
             <div className="App">
             <h1>Jotto</h1>
-            <Congrats success={true}></Congrats>
-            <GuessedWords guessedWords={[{ guessedWord:'train', letterMatchCount: 3}]}></GuessedWords>
+            <Congrats success={this.props.success}></Congrats>
+            <Input/>
+            <GuessedWords guessedWords={this.props.guessedWords}></GuessedWords>
             </div>
         );
     }
